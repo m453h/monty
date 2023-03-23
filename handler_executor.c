cf16a6a60c3b558da25015d2841c61e622776f65
Michael Hudson <mnkotagu@gmail.com>
2023-03-23 20:02:40 +0300

    Improve nop opcode

--- handler_executor.c
+++ handler_executor.c
@@ -18,7 +18,7 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 				{"pop", pop},
 				{"swap", swap},
 				{"add", add},
-				{"nop", nop}
+				{"nop", nop},
 				{NULL, NULL}
 				};
 	unsigned int i = 0;
