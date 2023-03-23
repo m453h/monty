fbc3203db61128d8d92c7a16bf649a9e37fb197f
Michael Hudson <mnkotagu@gmail.com>
2023-03-23 22:00:39 +0300

    Fix betty coding style issues

--- handler_executor.c
+++ handler_executor.c
@@ -12,7 +12,7 @@
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t instructions[] = {{"push", push}, {"pall", pall},
-				{"pint", pint},{"pop", pop},
+				{"pint", pint}, {"pop", pop},
 				{"swap", swap}, {"add", add},
 				{"nop", nop}, {"sub", sub},
 				{"div", _div}, {"mul", mul},
